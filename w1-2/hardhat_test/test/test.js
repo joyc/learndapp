f25c3d1db7e0d09581d2a7bd82f3f2ca401a6194
joyc <joyc@live.jp>
2022-02-28 23:38:45 +0900

    update w1-2

--- w1-2/hardhat_test/test/test.js
+++ w1-2/hardhat_test/test/test.js
@@ -1,14 +1,14 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Token contract", function () {
+describe("Counter contract", function () {
     it("Deployment should assign the total supply of tokens to the owner", async function () {
       const [owner] = await ethers.getSigners();
       const Counter = await ethers.getContractFactory("Counter");
   
       const counter = await Counter.deploy();
-  
-      const setTest = await counter.set(1);
+      
+      const setTest = await counter.add(1);
       expect(await counter.count()).to.equal(setTest);
     });
-  });
+});
